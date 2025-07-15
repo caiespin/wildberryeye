d6dce9002e6b70185da7392257426ddbe4d10cff
sophietao127 <sophietao127@gmail.com>
2025-07-14 23:54:39 -0700

    add scroll button to image carousel

--- src/raspberry5/frontend/src/components/EmblaCarousel.js
+++ src/raspberry5/frontend/src/components/EmblaCarousel.js
@@ -1,13 +1,21 @@
 // components/EmblaCarousel.js
-import React from "react";
+import React, { useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import "./Carousel.css";
 
 const EmblaCarousel = ({ images }) => {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
     Autoplay({ delay: 2000 }),
   ]);
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   const allImages = images;
 
   return (
@@ -19,6 +27,12 @@ const EmblaCarousel = ({ images }) => {
           </div>
         ))}
       </div>
+      <button className="embla__prev" onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className="embla__next" onClick={scrollNext}>
+        Next
+      </button>
     </div>
   );
 };
