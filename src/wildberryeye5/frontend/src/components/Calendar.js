0cca50e3780f63a2d577083408cfbaad816969d6
Sophie Tao <sophietao127@gmail.com>
2025-09-07 12:28:42 -0700

    change directory name

--- src/wildberryeye5/frontend/src/components/Calendar.js
+++ src/wildberryeye5/frontend/src/components/Calendar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import "../";
+import "..";
 
 function CalendarComp() {
   const [date, setDate] = useState(new Date());
